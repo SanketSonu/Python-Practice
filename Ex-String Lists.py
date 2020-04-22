fc92fc1121df83440a055e855c3ba575f8df9965
sanketsonu <sanketsonu321@gmail.com>
2020-04-22 15:43:04 +0530

    changes

--- Ex-String Lists.py
+++ Ex-String Lists.py
@@ -5,5 +5,3 @@ if(s==s[::-1]):
       print("The string is a palindrome")
 else:
       print("Not a palindrome")
-
-a = input("Please enter a word or number and we will show you the word or number in reverse order: \n")
