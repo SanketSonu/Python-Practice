c55fd185b8b726b65d2da4d3f495d0411a5329d4
sanketsonu <sanketsonu321@gmail.com>
2020-04-23 13:03:14 +0530

    update

--- Ex-String Lists.py
+++ Ex-String Lists.py
@@ -1,3 +1,7 @@
+"""
+Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+"""
+
 s = input("Please enter a word or number to check if its palindrome or not: \n")
 
 
